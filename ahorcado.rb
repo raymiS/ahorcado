56692d88cb52cb522ac431964975cc78f5607a66
Raymi Saldomando <raymi@able.co>
2018-05-28 13:45:55 -0300

    Add Missing Game Functionality
      Methods
        choose_character: while word_spaces_empty? is true the player has to choose a character
        insert_character: once the player has chosen a character, if the character is included in current_word, then we need to place said character in the position that corresponds with current_word help
    
      Change current_word, word_spaces and current_character to instance variables

--- ahorcado.rb
+++ ahorcado.rb
@@ -1,47 +1,62 @@
-=begin
-class Ahorcado
-  @@word_board = ["", "", "", ""]
-  
-  def initialize
+#TODO: word_spaces should depend from current_word
+# Array equivalent to the characters of the word 
+# [“”, “”, “”, “”, ...]
+# []
+# Array.new
+
+#current_word = "love"
+#word_spaces = Array.new(current_word.length, "")
+
+@current_word = "friends"
+@word_spaces = Array.new(@current_word.length, "")
+@current_characters = @current_word.split("")
+
+def display_game
+  @word_spaces.each do |characters|
+    print "| #{characters} | "
+  end
+  choose_character
+end
+
+def word_spaces_empty?
+  @word_spaces.all? { |w| w == "" }
+end
+
+def choose_character
+  while word_spaces_empty? do
     puts "Guess the word! Choose a letter"
-    @letter = gets.chomp
-  end
+    chosen_character = gets.chomp
 
-  def display_game(word_board)
-    word_board.each do |letter|
-      puts "| #{letter} | "
-    end
-  end
+    puts "#{chosen_character}"
 
-  def word_board_empty(word_board)
-    word_board.all? { |w| w == "" }
-  end
-end
-=end
-
-word_spaces = ["", "", "", ""]
-current_word = "love".split("")
-
-def display_game(word_board)
-  word_board.each do |letter|
-    print "| #{letter} | "
+    insert_character(chosen_character)
   end
 end
 
-def word_spaces_empty?(word_board)
-  word_board.all? { |w| w == "" }
-end
+def insert_character(chosen_character)
+  if @current_word.include?(chosen_character)
+    position = @current_characters.index(chosen_character)
+    @word_spaces[position] = chosen_character
 
-while word_board_empty(word_board) do
-  puts "Guess the word! Choose a letter"
-  chosen_letter = gets.chomp
-
-  if word.include?(letter)
-    puts "put the letter where it's suppossed to be"
+    puts "Word Spaces: #{@word_spaces}"
   else 
-    puts "nope, keep playin"
+    puts "Nope, keep playin"
   end
 end
 
-display_game(word_board)
-puts word_board_empty(word_board)
+display_game
+
+# current_word = “loop”
+# current_characters = [“l”, “o”, “v”, “e”]
+#  -> each_with_index   could be, but pushing it
+#  -> find_index        perhaps, but could bring trouble in the future
+#  -> index
+# words_spaces = [“”, “”, “”, “”]
+# chosen_character = “o”
+#
+# chosen_character *must appear* in word_spaces in the *position* that it holds in current_word
+# position = current_characters.index(chosen_character)
+# word_spaces[position] = chosen_character
+# word_spaces = [“”, “o”, “”, “”]
+
+#puts word_board_empty(word_board)
